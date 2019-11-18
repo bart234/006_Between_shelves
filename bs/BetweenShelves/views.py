78700275be349bd8e3a98c4e1ec236997a89d38e
bart234 <bartlomiej.p.biernacki@o2.pl>
2019-11-18 21:13:34 +0100

    create user form done

--- bs/BetweenShelves/views.py
+++ bs/BetweenShelves/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from BetweenShelves.froms import FormLogin, FormCreateUser
 from django.contrib import messages
 from django.contrib.auth.models import User
+from BetweenShelves.models import UserCfg
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
@@ -38,7 +39,35 @@ class Logout(View):
 class CreateUser(View):
     def get(self, request):
         f = FormCreateUser()
-        return render(request,'newuser.html', {'form':f})
+        return render(request,'user_new.html', {'form':f})
 
     def post(self, request):
-        pass
+        form = FormCreateUser(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            p1 = form.cleaned_data['pwd1']
+            p2 = form.cleaned_data['pwd2']
+            fname = form.cleaned_data['fname']
+            lname = form.cleaned_data['lanme']
+            email = form.cleaned_data['email']
+            mobile = form.cleaned_data['mobile']
+            u = User()
+            u.username = username
+            if p1 == p2:
+                u.set_password(p1)
+            else:
+                messages.warning(request, "Login or password is wrong. Please try again")
+                return redirect('Login')
+            u.first_name = fname
+            u.last_name = lname
+            u.email = email
+            u.save()
+            cfg = UserCfg(user=u,mobile=mobile)
+            cfg.save()
+            messages.success(request, "Account created, You can log in now")
+            return redirect('Login')
+
+
+        else:
+            messages.warning(request, "Login or password is wrong. Please try again")
+            return redirect('Login')
